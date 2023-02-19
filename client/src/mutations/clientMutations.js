4e38b8b5c51255b2756418478ea668c194499ca9
Rajesh KC <rajeshkcbars@gmail.com>
2023-02-19 14:32:34 +0545

    client mutation updated for apollo/client

--- client/src/mutations/clientMutations.js
+++ client/src/mutations/clientMutations.js
@@ -1,5 +1,14 @@
-// import { gql } from "@apollo/client";
+import { gql } from "@apollo/client";
 
-// const DELETE_CLIENT = gql`
-//     mutation deleteClient($id: ID)
-// `
+const DELETE_CLIENT = gql`
+    mutation deleteClient($id: ID!){
+        deleteClient(id: $id){
+            id
+            name
+            email
+            phone
+        }
+    }
+`;
+
+export { DELETE_CLIENT };
